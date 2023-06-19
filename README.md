0704fb63d9fae772d02177c824d28d311cf753fe
Michael Tidwell <tidwell@zebedee.io>
2023-06-19 15:39:00 -0400

    update for digits

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 # all-colors
-rust crate that returns a hex for a color name
+rust crate for converting a &str into a hex string
 
-For a full list of supported colors check colors.rs.
-Inputs are lowercased and sanitized before comparing.
 
-```rs
-use all_colors::get_color_hex;
+`get_color_hex` takes in a &str and will return a 6 char hex string.
 
-let color_hex = get_color_hex("aqua").expect("a color hex str for aqua");
+# Inputs this function will accept:
+- 6 digit hex string will return the original hex string. 
+- 3 digit hex will return 6 char hex string.
+- A standard color name from a list of roughly ~750 in colors.rs.
+- 9 digit decimal string, if over (0-255),(0-255),(0-255) will use 255.
+- Otherwise will sha256 hash the string and grab the first 3 bytes.
 
-println!("aqua is #{}", color_hex);
+```rust
+fn main() {
+    use all_colors::get_color_hex;
+
+    //EXAMPLES:
+    let color_hex = get_color_hex("aqua").expect("well known color name");
+    let color_hex2 = get_color_hex("give me something interesting").expect("some random color a4f662");
+    let color_hex3 = get_color_hex("f20").expect("f00 will return ff2200");
+    let color_hex4 = get_color_hex("010255001").expect("9 digits will become 0aff01");
+
+
+
+    println!("aqua is #{}", color_hex);
+}
+
+
+
+
+
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,73 @@
 pub mod colors;
+use sha2::{Digest, Sha256};
 
-pub fn get_color_hex(color: &str) -> Option<&str> {
+pub fn get_color_hex(color: &str) -> String {
     let color_key = sanitize_color_string(color);
     let binding = colors::get_colors();
-    let a = binding.get(color_key.as_str());
-    a.copied()
+    let color_lookedup = binding.get(color_key.as_str()).copied();
+
+    // if None see if we can do something clever to come up with a color
+    match color_lookedup {
+        Some(s) => s.to_string(),
+        None => come_up_with_color(&color_key),
+    }
 }
+
 fn sanitize_color_string(color: &str) -> String {
     let sanitized = color
         .chars()
-        .filter(|c| c.is_ascii_alphabetic())
+        .filter(|c| c.is_ascii_alphanumeric())
         .collect::<String>()
         .to_lowercase();
 
     sanitized
 }
+
+fn come_up_with_color(color: &str) -> String {
+    let mut result = String::with_capacity(6);
+    // check to see if the str is 3 or 6 length AND all chars are hex chars
+    if (color.len() == 6 || color.len() == 3) && color.chars().all(|c| c.is_ascii_hexdigit()) {
+        if color.len() == 3 {
+            for ch in color.chars() {
+                result.push(ch);
+                result.push(ch);
+            }
+        } else {
+            result = color.to_owned();
+        }
+        // Check to see if a bunch of numbers were given 0-255,0-255,0-255,
+    } else if color.chars().all(|c| c.is_ascii_digit()) && color.len() == 9 {
+        let part1 = &color[0..3];
+        let part2 = &color[3..6];
+        let part3 = &color[6..9];
+
+        let part1_num = part1.parse::<u8>().ok().unwrap_or(255);
+        let part2_num = part2.parse::<u8>().ok().unwrap_or(255);
+        let part3_num = part3.parse::<u8>().ok().unwrap_or(255);
+
+        let hex_part1 = format!("{:02x}", part1_num);
+        let hex_part2 = format!("{:02x}", part2_num);
+        let hex_part3 = format!("{:02x}", part3_num);
+
+        result = format!("{}{}{}", hex_part1, hex_part2, hex_part3);
+    } else {
+        // hash whatever we have then just return 6 hex chars for a color
+        let mut hasher = Sha256::new();
+        hasher.update(color);
+        let hresult = hasher.finalize();
+
+        let hex_string: String = hresult
+            .iter()
+            .take(3) // Take the first 3 bytes (6 hex characters)
+            .map(|byte| format!("{:02x}", byte))
+            .collect();
+
+        result = hex_string;
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use crate::get_color_hex;
@@ -22,21 +75,47 @@ mod tests {
     #[test]
     fn test_aqua() {
         let result = get_color_hex("aqua");
-        assert_eq!(result.unwrap(), "00ffff");
+        assert_eq!(result, "00ffff");
     }
     #[test]
     fn test_removing_bad_chars() {
-        let result = get_color_hex("aqua2[]");
-        assert_eq!(result.unwrap(), "00ffff");
+        let result = get_color_hex("aqua[]");
+        assert_eq!(result, "00ffff");
     }
     #[test]
     fn test_complicated() {
         let result = get_color_hex("Purple Mountain's Majesty");
-        assert_eq!(result.unwrap(), "9d81ba");
+        assert_eq!(result, "9d81ba");
     }
     #[test]
-    fn test_none() {
+    fn test_hasher() {
         let result = get_color_hex("snow white");
-        assert_eq!(result, None);
+        // will test "snowwhite"
+        assert_eq!(result, "b87df3");
+    }
+    #[test]
+    fn test_plainhex6() {
+        let result = get_color_hex("dabeef");
+        assert_eq!(result, "dabeef");
+    }
+    #[test]
+    fn test_plainhex3() {
+        let result = get_color_hex("1fa");
+        assert_eq!(result, "11ffaa");
+    }
+    #[test]
+    fn test_high_digits_truncating() {
+        let result = get_color_hex("999999999");
+        assert_eq!(result, "ffffff");
+    }
+    #[test]
+    fn test_digits() {
+        let result = get_color_hex("010255001");
+        assert_eq!(result, "0aff01");
+    }
+    #[test]
+    fn test_longrandomstring() {
+        let result = get_color_hex("give me something interesting");
+        assert_eq!(result, "a4f662");
     }
 }
